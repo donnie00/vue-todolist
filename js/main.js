c81cda1e48d12d15ca33e51469b697ed64d4a423
ippoRulez <ippolitodonato@gmail.com>
2022-11-23 20:43:22 +0100

    finished styling and added more logic to the page

--- js/main.js
+++ js/main.js
@@ -4,10 +4,13 @@ createApp({
 	data() {
 		return {
 			taskList: [
-				{text: 'Testo task da stampare 1', done: false},
+				{
+					text: 'Testo task da stampare 1',
+					done: false,
+				},
 				{text: 'Testo task da stampare 2', done: true},
-				{text: 'Testo task da stampare 3', done: false},
-				{text: 'Testo task da stampare 4', done: true},
+				{text: 'Testo task da stampare 3', done: true},
+				{text: 'Testo task da stampare 4', done: false},
 			],
 			newTaskData: {text: '', done: false},
 		};
@@ -24,7 +27,22 @@ createApp({
 			this.taskList.splice(index, 1);
 		},
 		addNewTask() {
-			this.taskList.push({...this.newTaskData});
+			const newText = this.newTaskData.text;
+
+			if (newText === '') {
+				alert('Inserire del testo perima di aggiungere la task');
+			} else {
+				this.taskList.push({text: newText});
+			}
+
+			this.newTaskData.text = '';
+		},
+
+		deleteAll() {
+			const del = confirm('Sicuro di voler eliminare tutte le task?');
+			if (del) {
+				this.taskList.splice(0, this.taskList.length);
+			}
 		},
 	},
 }).mount('#app');
